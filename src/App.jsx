370d98516e56b8999735a4b26e08b8fc428c124b
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-28 10:50:40 +0400

    Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
-import { About } from './components/About'
+// import { About } from './components/About'
 import { Navbar } from './components/Navbar'
 import { OrderSummary } from './components/OrderSummary'
 import { NoMatch } from './components/NoMatch'
@@ -11,6 +12,8 @@ import { Users } from './components/Users'
 import { UserDetails } from './components/UserDetails'
 import { Admin } from './components/Admin'
 
+const LazyAbout = React.lazy(() => import('./components/About'))
+
 function App() {
 
   return (
@@ -18,7 +21,14 @@ function App() {
     <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
+        {/* <Route path='about' element={<About />} /> */}
+        <Route path='about'
+          element={
+            <React.Suspense fallback='Loading...'>
+              <LazyAbout />
+            </React.Suspense>
+          }
+        />
         <Route path='order-summary' element={<OrderSummary />} />
         <Route path='products' element={<Products />}>
           <Route index element={<NewProducts />} />
