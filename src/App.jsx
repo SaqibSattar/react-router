2a3958ad5160ccd1166ec031c3c81582b5a3d4cc
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-28 10:13:02 +0400

    Index Route

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
         <Route path='products' element={<Products />}>
-          <Route index element={<FeaturedProducts />} />
+          <Route index element={<NewProducts />} />
           <Route path='featured' element={<FeaturedProducts />} />
           <Route path='new' element={<NewProducts />} />
         </Route>
